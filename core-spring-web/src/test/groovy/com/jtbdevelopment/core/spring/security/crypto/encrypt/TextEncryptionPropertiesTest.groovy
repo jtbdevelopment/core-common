f8afec7de3b94a332e6812bfc3457261fa26c57b
JTB <jtbdevelopment@gmail.com>
2015-01-08 06:37:58 -0500

    tests for text properties

--- core-spring-web/src/test/groovy/com/jtbdevelopment/core/spring/security/crypto/encrypt/TextEncryptionPropertiesTest.groovy
+++ core-spring-web/src/test/groovy/com/jtbdevelopment/core/spring/security/crypto/encrypt/TextEncryptionPropertiesTest.groovy
@@ -1,11 +1,21 @@
 package com.jtbdevelopment.core.spring.security.crypto.encrypt
 
+import org.springframework.context.annotation.Bean
+
+import javax.annotation.PostConstruct
+import java.lang.reflect.Method
+
 /**
  * Date: 1/2/15
  * Time: 6:11 PM
  */
 class TextEncryptionPropertiesTest extends GroovyTestCase {
     TextEncryptionProperties properties = new TextEncryptionProperties()
+
+    void testPostConstructAnnotation() {
+        Method m = TextEncryptionProperties.class.getMethod('testDefaults')
+        assert m.getAnnotation(PostConstruct.class)
+    }
 
     void testTestDefaultsGenerateWarning() {
         properties.password = 'NOTSET'
