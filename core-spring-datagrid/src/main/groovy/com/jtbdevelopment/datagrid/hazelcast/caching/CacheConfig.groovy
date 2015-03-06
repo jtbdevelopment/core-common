e19f8d5c057846ec81d99e2c2e3252ca1788d091
JTB <jtbdevelopment@gmail.com>
2015-03-05 20:47:35 -0500

    moved some of the hazelcast infra to core out of games

---
+++ core-spring-datagrid/src/main/groovy/com/jtbdevelopment/datagrid/hazelcast/caching/CacheConfig.groovy
@@ -0,0 +1,15 @@
+package com.jtbdevelopment.datagrid.hazelcast.caching
+
+import groovy.transform.CompileStatic
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.Configuration
+
+/**
+ * Date: 2/25/15
+ * Time: 7:13 AM
+ */
+@Configuration
+@EnableCaching
+@CompileStatic
+class CacheConfig {
+}
---
+++ core-spring-datagrid/src/test/groovy/com/jtbdevelopment/datagrid/hazelcast/HazelCastInstanceFactoryBeanTest.groovy
@@ -0,0 +1,28 @@
+package com.jtbdevelopment.datagrid.hazelcast
+
+import com.hazelcast.core.HazelcastInstance
+
+/**
+ * Date: 2/25/15
+ * Time: 7:00 PM
+ */
+class HazelCastInstanceFactoryBeanTest extends GroovyTestCase {
+    static HazelCastInstanceFactoryBean factoryBean = new HazelCastInstanceFactoryBean()
+
+    void testGetObject() {
+        assert factoryBean.object
+    }
+
+    void testGetObjectIsSame() {
+        factoryBean.setup()
+        assert factoryBean.object.is(factoryBean.object)
+    }
+
+    void testGetObjectType() {
+        assert HazelcastInstance.class.is(factoryBean.objectType)
+    }
+
+    void testIsSingleton() {
+        assert factoryBean.isSingleton()
+    }
+}
