597eedf37d491f96f91448916714ae164d4b88b8
JTB <jtbdevelopment@gmail.com>
2015-03-07 08:44:38 -0500

    renamed module from datagrid to hazelcast

---
+++ core-spring-hazelcast/src/test/groovy/com/jtbdevelopment/datagrid/hazelcast/HazelcastInstanceFactoryBeanTest.groovy
@@ -0,0 +1,44 @@
+package com.jtbdevelopment.datagrid.hazelcast
+
+import com.hazelcast.config.Config
+import com.hazelcast.core.HazelcastInstance
+
+/**
+ * Date: 2/25/15
+ * Time: 7:00 PM
+ */
+class HazelcastInstanceFactoryBeanTest extends GroovyTestCase {
+    HazelcastInstanceFactoryBean factoryBean = new HazelcastInstanceFactoryBean()
+
+    private static class Configurer implements HazelcastConfigurer {
+        boolean called = false
+
+        @Override
+        void modifyConfiguration(final Config config) {
+            assertNotNull config
+            called = true
+        }
+    }
+
+    void testGetObjectIsSame() {
+        factoryBean.setup()
+        assert factoryBean.object.is(factoryBean.object)
+        factoryBean.object.shutdown()
+    }
+
+    void testSetupCallsConfigurers() {
+        def configurers = [new Configurer(), new Configurer()]
+        factoryBean.configurers = configurers
+        factoryBean.setup()
+        assert factoryBean.object
+        assertNull configurers.find { !it.called }
+    }
+
+    void testGetObjectType() {
+        assert HazelcastInstance.class.is(factoryBean.objectType)
+    }
+
+    void testIsSingleton() {
+        assert factoryBean.isSingleton()
+    }
+}
