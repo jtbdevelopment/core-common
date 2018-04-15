f04150564c0cf95252077a549579ed28c75a02b7
JTB <jtbdevelopment@gmail.com>
2018-04-14 21:42:38 -0400

    removing groovy

---
+++ core-spring-jackson/src/main/java/com/jtbdevelopment/spring/jackson/AutoRegistrableJsonSerializer.java
@@ -0,0 +1,10 @@
+package com.jtbdevelopment.spring.jackson;
+
+import com.fasterxml.jackson.databind.JsonSerializer;
+
+/**
+ * Date: 1/13/15 Time: 7:46 PM
+ */
+public abstract class AutoRegistrableJsonSerializer<T> extends JsonSerializer<T> {
+
+}
