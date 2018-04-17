dcf0257373ec135291d1b8743a6ff906749abb75
JTB <jtbdevelopment@gmail.com>
2018-04-17 08:39:43 -0400

    removing test groovy

--- core-spring-quartz/src/test/java/com/jtbdevelopment/core/spring/quartz/AutowiredSchedulerFactoryBeanTest.java
+++ core-spring-quartz/src/test/java/com/jtbdevelopment/core/spring/quartz/AutowiredSchedulerFactoryBeanTest.java
@@ -1,11 +1,12 @@
 package com.jtbdevelopment.core.spring.quartz;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
 import java.util.List;
 import org.junit.Test;
-import org.mockito.Mockito;
 import org.quartz.impl.triggers.CronTriggerImpl;
 import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
 import org.springframework.test.util.ReflectionTestUtils;
@@ -21,10 +22,10 @@ public class AutowiredSchedulerFactoryBeanTest {
   public void testConstructor() {
     CronTriggerImpl t1 = new CronTriggerImpl();
     CronTriggerImpl t2 = new CronTriggerImpl();
-    CronTriggerFactoryBean tf1 = Mockito.mock(CronTriggerFactoryBean.class);
-    Mockito.when(tf1.getObject()).thenReturn(t1);
-    CronTriggerFactoryBean tf2 = Mockito.mock(CronTriggerFactoryBean.class);
-    Mockito.when(tf2.getObject()).thenReturn(t2);
+    CronTriggerFactoryBean tf1 = mock(CronTriggerFactoryBean.class);
+    when(tf1.getObject()).thenReturn(t1);
+    CronTriggerFactoryBean tf2 = mock(CronTriggerFactoryBean.class);
+    when(tf2.getObject()).thenReturn(t2);
     schedulerFactoryBean = new AutowiredSchedulerFactoryBean(Arrays.asList(tf1, tf2));
     assertArrayEquals(Arrays.asList(t1, t2).toArray(),
         ((List) ReflectionTestUtils.getField(schedulerFactoryBean, "triggers")).toArray());
